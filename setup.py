81fc8907e3448e2bb34bb5019a6d2d779835905f
Benjamin Piwowarski <benjamin@bpiwowar.net>
2017-10-05 11:40:20 +0200

    Use proper entry point

--- setup.py
+++ setup.py
@@ -3,11 +3,16 @@
 from distutils.core import setup
 
 setup(name='datasets',
-      version='0.1',
-      description='Datasets utilities',
-      author='Benjamin Piwowarski',
-      author_email='benjamin@bpiwowar.net',
-      url='https://github.com/bpiwowar/datasets',
-      packages=['datasets'],
-      package_dir={'datasets': 'datasets'},
+    version='0.1',
+    description='Datasets utilities',
+    author='Benjamin Piwowarski',
+    author_email='benjamin@bpiwowar.net',
+    url='https://github.com/bpiwowar/datasets',
+    packages=['datasets'],
+    package_dir={'datasets': 'datasets'},
+    entry_points = {
+        'console_scripts': [
+            'datasets = datasets.__main__:main',                  
+        ],              
+    },
 )
