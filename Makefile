a7ad802d931c21f1bd31c0ad5bb3404c6833a76c
Benjamin Piwowarski <benjamin@piwowarski.fr>
2019-02-07 13:56:20 +0100

    Added circleci

---
+++ Makefile
@@ -0,0 +1,26 @@
+.PHONY: help clean dev docs package test
+
+help:
+	@echo "This project assumes that an active Python virtualenv is present."
+	@echo "The following make targets are available:"
+	@echo "	 dev 	install all deps for dev env"
+	@echo "  docs	create documentation"
+	@echo "	 test	run all tests with coverage"
+
+clean:
+	rm -rf dist/*
+
+dev:
+	pip install -r requirements.txt
+	pip install -r requirements-dev.txt
+	pip install -e .
+
+docs:
+	$(MAKE) -C docs html
+
+package:
+	python setup.py sdist
+
+test:
+	coverage run -m unittest discover
+	coverage html
--- setup.py
+++ setup.py
@@ -1,16 +1,41 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_namespace_packages
+from setuptools.command.install import install
+import re
+
+VERSION='0.2'
+
+RE_BLANCK=re.compile(r"^\s*#?")
+with open('requirements.txt') as f:
+    requirements = [x for x in f.read().splitlines() if not RE_BLANCK.match(x)]
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
 
 setup(name='datamaestro',
-    version='0.1',
+    version=VERSION,
     description='Dataset management',
     author='Benjamin Piwowarski',
     author_email='benjamin@piwowarski.fr',
     url='https://github.com/bpiwowar/datamaestro',
     packages=find_namespace_packages(include="datamaestro.*"),
-    install_requires = [ 'Click', 'tqdm', 'pyyaml' ],
+    install_requires = requirements,
     package_data={'datamaestro': ['LICENSE', 'datamaestro/repositories.yaml']},
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    },
+    python_requires='>=3',
     data_files = [
         
     ],
